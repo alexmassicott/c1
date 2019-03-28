5848722352db764f0fe23ee070f7e9ee031590df
Alexander  Massicott Jr <bxgoods@aol.com>
2019-03-27 21:17:07 -0400

    no message

---
+++ src/measurements/measurement-store.js
@@ -0,0 +1,32 @@
+import { Measurement } from './measurement';
+import { HttpError } from '../errors';
+
+/**
+ * DB to store measurements
+ */
+const db = [];
+
+/**
+ * Add new measurement
+ * @param {Measurement} measurement to be added
+ */
+export function add(measurement) {
+  db.push(measurement);
+}/**
+ * Get existing measurement
+ * @param {Date} timestamp when measurement was taken
+ * @returns {Measurement} measurement for the particular date
+ */
+export function fetch(timestamp) {
+  const result = db.find(measurement => measurement.getTimestamp().getTime() == new Date(timestamp).getTime());
+  return result;
+}
+
+/**
+ * Get the measurements within the given date range
+ * @param {Date} start Lower bound for the query, inclusive
+ * @param {Date} end Upper bound for the query, exclusive
+ */
+export function queryDateRange(from, to) {
+  throw new HttpError(501);
+}
---
+++ src/measurements/measurement.js
@@ -0,0 +1,29 @@
+/**
+ * @property {Date} timestamp
+ */
+export class Measurement {
+  constructor() {
+    /** @private */
+    this.metrics = new Map();
+  }
+
+  /**
+   * @param {String} name
+   * @param {Number} value
+   */
+  setMetric(name, value) {
+    this.metrics.set(name, value);
+  }
+
+  /**
+   * @param {String} name
+   * @return {Number}
+   */
+  getMetric(name) {
+    return this.metrics.get(name);
+  }
+
+  getTimestamp() {
+    return this.timestamp;
+  }
+}
